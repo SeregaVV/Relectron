455ff20a8b176ccf71034c46303588626102ce86
Воронцов Сергей Валерьевич <Voroncov.SV@partner.ru>
2016-12-22 17:10:56 +1000

    index.js refactored

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const {app, BrowserWindow} = require('electron');
 const path = require('path')
 const url = require('url')
 
-let win;
+let mainWindow;
 
 app.on('ready',()=>{
-  win = new BrowserWindow({width: 800, height: 600})
-  win.loadURL(url.format({
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL(url.format({
     pathname: path.join(__dirname,'index.html'),
     protocol:'file',
     slashes:true
   }));
-  win.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('closed',()=>{
+      mainWindow = null;
+  })
 });
